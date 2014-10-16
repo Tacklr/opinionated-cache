11f7479bd2f02ae927b0bf0969fe3b612ae476d7
Marc Brooks <IDisposable@gmail.com>
2014-10-15 20:55:41 -0500

    No longer a portable class library.

--- src/OpinionatedCache/Properties/AssemblyInfo.cs
+++ src/OpinionatedCache/Properties/AssemblyInfo.cs
@@ -1,14 +1,16 @@
 ﻿using System.Reflection;
 using System.Resources;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("OpinionatedCache Portable")]
+[assembly: AssemblyTitle("OpinionatedCache")]
 [assembly: AssemblyDescription("A class library that standardizes the cache managment policies and keys for caching a backing store")]
-[assembly: AssemblyConfiguration("Portable Class Library")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TacklR")]
 [assembly: AssemblyProduct("OpinionatedCache")]
 [assembly: AssemblyCopyright("Copyright © TacklR 2010-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
+[assembly: Guid("7d73af00-f0fb-4938-ad32-8b4ff7026e55")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
